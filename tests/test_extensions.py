6430206d15c18ed16732098c1d3fa4099bc49edd
Phil Schaf <flying-sheep@web.de>
2026-04-27 16:36:14 +0200

    whoops

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -210,7 +210,7 @@ def test_missing_annotation() -> None:
 
         @register_dummy_namespace("missing_annotation")
         class MissingAnnotationNamespace:
-            def __init__(self, obj: object) -> None:
+            def __init__(self, obj) -> None:  # type: ignore[no-untyped-def]
                 self.obj = obj
 
 
