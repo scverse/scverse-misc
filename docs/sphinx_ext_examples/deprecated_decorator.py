7602ecd590e401d766196fa4e70666a23700882f
Philipp A. <flying-sheep@web.de>
2026-06-17 15:27:16 +0200

    style: annotation lints

--- docs/sphinx_ext_examples/deprecated_decorator.py
+++ docs/sphinx_ext_examples/deprecated_decorator.py
@@ -2,11 +2,10 @@ from scverse_misc import deprecated, Deprecation
 
 
 @deprecated(Deprecation("0.3.1", "Use bar instead."))
-def foo(a: int, b: str):
+def foo(a: int, b: str) -> None:
     """Frobnicates its arguments.
 
     Args:
         a: The frobnicator.
         b: The frobnicatee.
     """
-    pass
