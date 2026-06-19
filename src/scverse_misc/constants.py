5bdc78b5f5a4c31090d68ee9869808eb7255840a
Philipp A. <flying-sheep@web.de>
2026-06-19 15:50:12 +0200

    Update src/scverse_misc/constants.py
    Co-authored-by: ilia-kats <ilia-kats@gmx.net>

--- src/scverse_misc/constants.py
+++ src/scverse_misc/constants.py
@@ -1,6 +1,6 @@
 from typing import Final
 
-__all__ = ["ATTR_NAMESPACE"]
+__all__ = ["ATTR_NAMESPACE", "ATTR_DEPRECATED", "ATTR_DEPRECATED_ARG"]
 
 ATTR_NAMESPACE: Final = "__scverse_misc_namespace_info__"
 ATTR_DEPRECATED: Final = "__deprecated__"
